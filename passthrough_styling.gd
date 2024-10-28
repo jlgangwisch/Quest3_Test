2004326120d6431231cf95717b4bb9467d29f66e
unknown <28738403+jlgangwisch@users.noreply.github.com>
2024-10-28 18:17:22 -0400

    updated passthrough code

--- passthrough_styling.gd
+++ passthrough_styling.gd
@@ -2,10 +2,16 @@ extends Node
 
 #styling guide: https://godotvr.github.io/godot_openxr_vendors/manual/meta/passthrough.html
 var fb_passthrough: Object = Engine.get_singleton("OpenXRFbPassthroughExtensionWrapper")
-enum STYLE_TYPE { COLOR_MAP , MONO_MAP , BCS , COLOR_LUT, INTERPOLATED_COLOR_LUT, DISABLES}
-@export var style :STYLE_TYPE :
+enum STYLE_TYPE { COLOR_MAP , MONO_MAP , BCS , COLOR_LUT, INTERPOLATED_COLOR_LUT, DISABLED}
+
+@export var update_passthrough:= false
+
+@export var style :STYLE_TYPE  = 5 : 
 	set(value):
+		if style != value:
+			print("style changed: ", style)
 		style = value
+		
 		match style:
 			0:
 				fb_passthrough.set_color_map(color_map)
@@ -14,27 +20,52 @@ enum STYLE_TYPE { COLOR_MAP , MONO_MAP , BCS , COLOR_LUT, INTERPOLATED_COLOR_LUT
 			2:
 				fb_passthrough.set_brightness_contrast_saturation(brightness, contrast, saturation)
 			3: 
-				var source : OpenXRMetaPassthroughColorLut = fb_passthrough.create_from_image(source_lut, channels)
-				fb_passthrough.set_color_lut(lut_weight, source)
+				if source_lut and channels:
+					var source : OpenXRMetaPassthroughColorLut = fb_passthrough.create_from_image(source_lut, channels)
+					fb_passthrough.set_color_lut(lut_weight, source)
 				
 			4: 
-				var source : OpenXRMetaPassthroughColorLut = fb_passthrough.create_from_image(source_lut, channels)
-				var target : OpenXRMetaPassthroughColorLut = fb_passthrough.create_from_image(target_lut, channels)
-				fb_passthrough.set_interpolated_color_lut(lut_weight, source, target)
+				if source_lut and channels and target_lut:
+					var source : OpenXRMetaPassthroughColorLut = fb_passthrough.create_from_image(source_lut, channels)
+					var target : OpenXRMetaPassthroughColorLut = fb_passthrough.create_from_image(target_lut, channels)
+					fb_passthrough.set_interpolated_color_lut(lut_weight, source, target)
 				
 			5:
 				fb_passthrough.set_passthrough_filter(0) #0 is disabled
 				
 @export_category("Color Map")
-@export var color_map : Gradient
+@export var color_map : Gradient : 
+	set(value):
+		color_map = value
+		#set_style(STYLE_TYPE.COLOR_MAP)
+		style = style
+		fb_passthrough.set_color_map(color_map)
 
 @export_category("Mono Map")
-@export var mono_map : Curve
+@export var mono_map : Curve : 
+	set(value):
+		mono_map = value
+		#set_style(style)
+		style = style
+		fb_passthrough.set_mono_map(mono_map)
+
 
 @export_category("BCS")
-@export_range (-100, 100, 0.01) var brightness : float = 0 #neutral is 0
-@export_range (0, 10, 0.01) var contrast : float = 0 #neutral is 1
-@export_range (0, 10, 0.01) var saturation : float = 0 #neutral is 1
+@export_range (-100, 100, 0.01) var brightness : float = 0 :#neutral is 0
+	set(value):
+		brightness = value
+		#set_style(style)
+		style = style
+@export_range (0, 10, 0.01) var contrast : float = 1 : #neutral is 1
+	set(value):
+		contrast = value
+		#set_style(style)
+		style = style
+@export_range (0, 10, 0.01) var saturation : float = 1 :#neutral is 1
+	set(value):
+		saturation = value
+		#set_style(style)
+		style = style
 
 @export_category("Color LUT")
 enum LUT_CHANNELS {RGB, RGBA}
@@ -71,12 +102,17 @@ enum LUT_CHANNELS {RGB, RGBA}
 
 
 func _ready() -> void:
+	style = style
+	fb_passthrough.set_mono_map(mono_map)
 	#update_lut()
 	#style = style
 	#edge_color=edge_color
 	#texture_opacity = texture_opacity
 	pass
 	
+func _process(delta: float) -> void:
+	if update_passthrough:
+		style = style
 
 	
 func update_lut()->void:
